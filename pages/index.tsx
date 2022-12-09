926e81efc8d050c1ff8d4491fac9792ecc5025f8
sospeter <sospeterkaranja02@gmail.com>
2022-12-09 17:03:17 +0300

    initiall commit

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex min-h-screen">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="">
+        dfghfp
+      </main>
+
+    </div>
+  )
+}
+
+export default Home
